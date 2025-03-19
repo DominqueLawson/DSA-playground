b28bd25f24db9a682caf0902b5ca60256410cbed
DominqueLawson <dominque.lawson115@gmail.com>
2025-03-19 12:58:32 -0700

    inital commit with binary search example

---
+++ test/test_binary_search.py
@@ -0,0 +1,5 @@
+from algorithms.binary_search import binary_search
+
+def test_binary_search():
+    assert binary_search([1, 2, 3, 4, 5], 3) == 2
+    assert binary_search([1, 2, 3, 4, 5], 6) == -1
